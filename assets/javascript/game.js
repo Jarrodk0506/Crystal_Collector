e160e87a47e20cb7190790fa26e50ebc07e261a4
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-07-02 17:01:03 -0400

    Finshed layout. Successfully display score wins and target score. Turned crystal images into buttons. Assigned random number between 1-12 to button. Button scores tally in your score. Need to work on evaluating score = target

---
+++ assets/javascript/game.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+
+
+var target;
+var score= 0;              //global variables
+var wins= 0;
+var geodeValue= 0;
+var amethystValue= 0;
+var iceValue= 0;
+var diamondValue= 0;
+
+function Start(){           //<-- Start game
+score = 0;
+wins = 0;       //reset your score and wins
+    
+//gather values for the target value and set each button value
+target = [Math.floor(Math.random() * 101 ) + 19];
+geodeValue = [Math.floor(Math.random() * 12 ) + 1];
+amethystValue = [Math.floor(Math.random() * 12 ) + 1];
+iceValue = [Math.floor(Math.random() * 12 ) + 1];
+diamondValue = [Math.floor(Math.random() * 12 ) + 1];
+
+//display wins , Your score and the score to match
+var winDisplay = $('#wins').text(wins);
+var scoreDisplay = $('#score').text(score);
+var targetDisplay = $("#target").text(target);
+
+//checks
+console.log(geodeValue);
+console.log(amethystValue);
+console.log(iceValue);
+console.log(diamondValue);
+console.log(target);
+
+////////// NEED HELP EVALUATING SCORE TO TARGET ///////////
+
+//geode button
+$('.geode').on('click', function(){
+        score = parseInt(score) + parseInt(geodeValue);
+        var scoreDisplay = $('#score').text(score);
+        if(score === target){
+            alert("You win!");
+            wins++
+        }
+        else if(score > target){
+            alert("You lose!");
+        }
+});
+
+//amethyst button
+$('.amethyst').on('click', function(){     
+        score = parseInt(score) + parseInt(amethystValue);
+        var scoreDisplay = $('#score').text(score);
+        if(score === target){
+            alert("You win!");
+            wins++
+        }
+        else if(score > target){
+            alert("You lose!");
+        }
+});
+
+//ice button
+$('.ice').on('click', function(){
+    score = parseInt(score) + parseInt(iceValue);
+    var scoreDisplay = $('#score').text(score);
+    if(score === target){
+        alert("You win!");
+        wins++
+    }
+    else if(score > target){
+        alert("You lose!");
+    }
+});
+
+//diamond button
+$('.diamond').on('click', function(){ 
+    score = parseInt(score) + parseInt(diamondValue);
+    var scoreDisplay = $('#score').text(score);
+    if(score === target){
+        alert("You win!");
+        wins++
+    }
+    else if(score > target){
+        alert("You lose!");
+    }
+});
+
+
+
+
+}
+
+
+Start();
+
+});
